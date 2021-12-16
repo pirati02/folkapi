1dad8b1ecc8457a6dbf474cbb7d19de6151aa17f
pirati02 <b.gogia94@gmail.com>
2021-12-16 12:05:58 +0400

    s

--- webpack.config.js
+++ webpack.config.js
@@ -30,10 +30,5 @@ module.exports = {
       }
     ]
   },
-  plugins:  [
-    "@babel/plugin-transform-runtime",
-    {
-      "regenerator": true
-    }
-  ]
+  plugins:  ["@babel/plugin-transform-runtime"]
 }
